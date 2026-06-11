2d59a1b00ee39749dcb22b2278ff14a93b1a5487
Ramon van Bruggen <ramonvanbruggen@gmail.com>
2026-06-11 21:16:24 +0200

    feat: add coverage native phase0 build

---
+++ research/coverage-native/compat/ros/assert.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cassert>
+
+#define ROS_ASSERT(condition) assert(condition)
+#define ROS_ASSERT_MSG(condition, ...) assert(condition)
+
---
+++ research/coverage-native/compat/ros/console.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define ROS_DEBUG(...) ((void)0)
+#define ROS_INFO(...) ((void)0)
+#define ROS_WARN(...) ((void)0)
+#define ROS_ERROR(...) ((void)0)
+#define ROS_FATAL(...) ((void)0)
+
+#define ROS_DEBUG_STREAM(message) ((void)0)
+#define ROS_INFO_STREAM(message) ((void)0)
+#define ROS_WARN_STREAM(message) ((void)0)
+#define ROS_ERROR_STREAM(message) ((void)0)
+#define ROS_FATAL_STREAM(message) ((void)0)
+
+#define ROS_DEBUG_COND(condition, ...) ((void)0)
+#define ROS_INFO_COND(condition, ...) ((void)0)
+#define ROS_WARN_COND(condition, ...) ((void)0)
+#define ROS_ERROR_COND(condition, ...) ((void)0)
+#define ROS_FATAL_COND(condition, ...) ((void)0)
+
+#define ROS_DEBUG_STREAM_COND(condition, message) ((void)0)
+#define ROS_INFO_STREAM_COND(condition, message) ((void)0)
+#define ROS_WARN_STREAM_COND(condition, message) ((void)0)
+#define ROS_ERROR_STREAM_COND(condition, message) ((void)0)
+#define ROS_FATAL_STREAM_COND(condition, message) ((void)0)
+
